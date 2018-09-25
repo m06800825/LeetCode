deaeb77d4b2c63e00bdb7011b2a40dad13c512e6
Kai-Yuan, Cheng <40761610+m06800825@users.noreply.github.com>
2018-09-25 19:41:19 +0800

    38

---
+++ question38.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        result = '1'
+        for _ in range(n-1):
+            prev = result
+            result = ''
+            j = 0
+            while j < len(prev):
+                cur = prev[j]
+                cnt = 1
+                j += 1
+                while j < len(prev) and prev[j] == cur:
+                    cnt += 1
+                    j += 1
+                result += str(cnt) + str(cur)
+        return result
